cf066e8a6dd60ede3e8749433e5322687f91d903
BinhTran <fam.out16@gmail.com>
2024-08-28 00:42:45 +0700

    Add cac files bai thuc hanh ve static method

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace LOPVADOITUONG
+{
+    public class Student
+    {
+        int rollno;
+        string name;
+        static string college = "ABC";
+        public Student(int r, string n)
+        {
+            name = n;
+            rollno = r;
+        }
+        public static void Change()
+        {
+            college = "CODEGYM";
+        }
+        public void Display()
+        {
+            Console.WriteLine($"{rollno} {name} {college}");
+        }
+    }
+    public class TestStaticMethod
+    {
+        public static void Main(string[] args)
+        {
+            Student.Change();
+            Console.WriteLine("Enter rollno and name:");
+            int rn = Convert.ToInt32(Console.ReadLine());
+            string n = Console.ReadLine();
+            Student student = new Student(rn, n);
+            student.Display();
+        }
+    }
+}
